70ee466dc495192aa28b6730e85231a916f4fe65
thành <vinhthanhluong@gmail.com>
2021-11-17 17:31:49 +0700

    update 17-11

--- template/js/modules/FileActiveModule.js
+++ template/js/modules/FileActiveModule.js
@@ -4,4 +4,12 @@ export default function FileActiveModule() {
     $('.tab-link-mobi').on('click', function () {
         $('.tab-link').slideToggle();
     });
+
+    if ($('.warranty-policy')) {
+        $('.policy-tt').on('click', function () {
+            $(this).parent('.policy-box').toggleClass('active');
+            $(this).next('.policy-content').slideToggle();
+        });
+        
+    }
 }
--- template/js/modules/SlideModule.js
+++ template/js/modules/SlideModule.js
@@ -101,6 +101,53 @@ export default function SlideModule(){
     })
 
 
+    if (document.querySelector('.swiper-cata')) {
+        const swiper = document.querySelector('.swiper-cata');
+        const sliderContainer = swiper.querySelector('.swiper');
+        const SliderPagination = swiper.querySelector('.swiper-pagination');
+        const sliderPrevBtn = swiper.querySelector('.swiper-button-prev');
+        const sliderNextBtn = swiper.querySelector('.swiper-button-next');
+        try {
+            const swiper = new Swiper(sliderContainer, {
+                speed: 1000,
+                    loop: true,
+                    spaceBetween: 0,
+                    slidesPerView: 1,
+                    // autoplay: {
+                    //     delay: 4000,
+                    // },
+                    pagination: {
+                        el: '.swiper-pagination',
+                        clickable: true,
+                    },
+
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+
+                    breakpoints: {
+                        // 320: {
+                        //     spaceBetween: 50
+                        // },
+                        // 480: {
+                        //     spaceBetween: 30
+                        // },
+                        // 576: {
+                        //     spaceBetween: 0,
+
+                        // }
+                    }
+                    // observeParents:true,
+                    // observeSlideChildren: true,
+                    // observer: true,
+            })
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
+
     if (document.querySelector('.swiper-demo')) {
         const swiper = document.querySelector('.swiper-demo');
         const sliderContainer = swiper.querySelector('.swiper');
